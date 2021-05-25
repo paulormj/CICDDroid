ecb614a3862f33521b762b78fc7e5717747ad8d5
Paulo Menezes <paulormj@pbh.gov.br>
2021-05-25 20:55:27 -0300

    initial commit

---
+++ app/src/main/java/com/lalorosas/retirementcalculator/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.lalorosas.retirementcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "6ea11f14-e195-449a-8559-bca4ec3727e5", Analytics::class.java, Crashes::class.java);
+
+        calculateButton.setOnClickListener {
+            //throw Exception("Algo deu errado");
+            //Crashes.generateTestCrash();
+         try {
+             val taxaDeJuros = interestEditText.text.toString().toFloat();
+             val idadeAtual = ageEditText.text.toString().toInt();
+             val idadeAposentar = retirementEditText.text.toString().toInt();
+             val aplicacaoMensal = monthlySavingsEditText.text.toString().toFloat()
+             val aplicacaoAtual = currentEditText.text.toString().toFloat();
+
+             val properties: HashMap<String, String> = HashMap<String, String>()
+             properties.put("taxaDeJuros", taxaDeJuros.toString())
+             properties.put("idadeAtual", idadeAtual.toString())
+             properties.put("idadeAposentar", idadeAposentar.toString())
+             properties.put("aplicacaoMensal", aplicacaoMensal.toString())
+             properties.put("aplicacaoAtual", aplicacaoAtual.toString())
+
+             if (taxaDeJuros <= 0) {
+                 Analytics.trackEvent("taxa_de_juros_errada",properties);
+             }
+             if (idadeAposentar <= idadeAtual) {
+                 Analytics.trackEvent("idade_errada",properties);
+             }
+
+         }catch (ex: Exception){
+             Analytics.trackEvent(ex.message);
+         }
+      }
+    }
+}
