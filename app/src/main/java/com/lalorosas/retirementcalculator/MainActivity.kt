3f50c9562c85c02deb4c1473a86a3aebb97a5e69
Paulo Menezes <paulormj@pbh.gov.br>
2021-05-27 22:26:26 -0300

    Feature-5

--- app/src/main/java/com/lalorosas/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/lalorosas/retirementcalculator/MainActivity.kt
@@ -2,11 +2,14 @@ package com.lalorosas.retirementcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.utils.async.AppCenterConsumer
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,37 +17,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //UPdate Secret
-        AppCenter.start(application, "d7672144-5299-4b9d-abbe-c55e69a90456", Analytics::class.java, Crashes::class.java);
+        AppCenter.start(application, "dad01173-aa9d-43d8-b7ea-df1b7bec99b0", Analytics::class.java, Crashes::class.java)
+
+        val future = Crashes.hasCrashedInLastSession()
+        future.thenAccept(AppCenterConsumer {
+            if(it){
+                Toast.makeText(this, "Oops! Sorry about that!", Toast.LENGTH_LONG).show()
+            }
+        })
 
         calculateButton.setOnClickListener {
-            //throw Exception("Algo deu errado");
-            //Crashes.generateTestCrash();
-         try {
-             val taxaDeJuros = interestEditText.text.toString().toFloat();
-             val idadeAtual = ageEditText.text.toString().toInt();
-             val idadeAposentar = retirementEditText.text.toString().toInt();
-             val aplicacaoMensal = monthlySavingsEditText.text.toString().toFloat()
-             val aplicacaoAtual = currentEditText.text.toString().toFloat();
+             Crashes.generateTestCrash()
+            try {
+                val interestRate = interestEditText.text.toString().toFloat()
+                val currentAge = ageEditText.text.toString().toInt()
+                val retirementAge = retirementEditText.text.toString().toInt()
+                val monthly = monthlySavingsEditText.text.toString().toFloat()
+                val current = currentEditText.text.toString().toFloat()
 
-             val properties: HashMap<String, String> = HashMap<String, String>()
-             properties.put("taxaDeJuros", taxaDeJuros.toString())
-             properties.put("idadeAtual", idadeAtual.toString())
-             properties.put("idadeAposentar", idadeAposentar.toString())
-             properties.put("aplicacaoMensal", aplicacaoMensal.toString())
-             properties.put("aplicacaoAtual", aplicacaoAtual.toString())
+                val properties:HashMap<String, String> = HashMap<String, String>()
+                properties.put("interest_rate", interestRate.toString())
+                properties.put("current_age", currentAge.toString())
+                properties.put("retirement_age", retirementAge.toString())
+                properties.put("monthly_savings", monthly.toString())
+                properties.put("current_savings", current.toString())
 
-             if (taxaDeJuros <= 0) {
-                 Analytics.trackEvent("taxa_de_juros_errada",properties);
-             }
-             if (idadeAposentar <= idadeAtual) {
-                 Analytics.trackEvent("idade_errada",properties);
-             }
-             resultTextView.text = "At the current rate of  $taxaDeJuros, saving \$$aplicacaoMensal a month"
+                if (interestRate <= 0) {
+                    Analytics.trackEvent("wrong_interest_rate", properties)
+                }
+                if (retirementAge <= currentAge) {
+                    Analytics.trackEvent("wrong_age", properties)
+                }
 
-         }catch (ex: Exception){
-             Analytics.trackEvent(ex.message);
-         }
-      }
+                val futureSavings = calculateRetirement(interestRate, current, monthly, (retirementAge - currentAge)*12)
+
+                resultTextView.text = "At the current rate of $interestRate%, saving \$$monthly a month you will have \$${String.format("%f", futureSavings)} by $retirementAge."
+            } catch(ex: Exception){
+                Analytics.trackEvent(ex.message)
+            }
+        }
+    }
+
+    fun calculateRetirement(interestRate: Float, currentSavings: Float, monthly: Float, numMonths: Int): Float {
+        var futureSavings = currentSavings * (1+(interestRate/100/12)).pow(numMonths)
+
+        for (i in 1..numMonths) {
+            futureSavings += monthly * (1+(interestRate/100/12)).pow(i)
+        }
+
+        return  futureSavings
     }
 }
